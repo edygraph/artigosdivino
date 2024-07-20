2626ea4b464ec5e3399f1ca30b0b1cc3e083ef98
qualitdesign <contato.qualitdesign@gmail.com>
2024-07-20 13:17:54 -0300

    Quartz sync: Jul 20, 2024, 1:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Artigos Divino",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "DM Serif Display",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
